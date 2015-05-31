9e151f99ad7df9b7221c81aa6a3a9c5eaee26fdd
Constanti <constantin.dumba@gmail.com>
2015-05-30 14:12:45 -1000

    done

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,14 @@
 'use strict';
 module.exports = function() {
-  
+  var randomNumber = Math.floor((Math.random()*100)+1);
+  return function(){
+    return randomNumber;
+  }
 };
+
+var secretNumberGenerator = module.exports;
+var secretNumber = secretNumberGenerator();
+var generatedNumber = secretNumber();
+var generatedNumber1 = secretNumberGenerator()();
+var generatedNumber2= secretNumberGenerator()();
+var originalNumber = secretNumber();
